c31498ea8c3f0acf2acd91796f4e219fd8b192fb
Brayan3000750 <brayanpinel030@gmail.com>
2024-11-08 15:22:50 -0600

    en la funcion de clientes y canales nos permite  ingresar el nombre de un cliente, tipos de canales (HD o NORMAL) y el tipo de caja digital (LIGHTBOX, HDBOX, DVRBOX). Cuenta la cantidad de canales HD y normales.

--- src/examenlaboratorio/Brayan_PinelE1.java
+++ src/examenlaboratorio/Brayan_PinelE1.java
@@ -54,26 +54,92 @@ int suma=0;
 
 
 
+for (int j = 0; j < x; j++) {
+                System.out.print(numero + " ");
+                suma += numero;
+                numero += 2;
+            }
+            System.out.println("= " + suma);
+        }
     
 }
 
 public static void MayorYPromedio(){
 Scanner datos=new Scanner(System.in);
-int max=Integer.MIN_VALUE;
+int maximo=Integer.MIN_VALUE;
 int sume=0;
 int contar=0;
 
 String respuesta;
-
+do{
 System.out.println("Ingrese un numero");
-int num =datos.nextInt();
+int numeros =datos.nextInt();
+
+sume += numeros;
+            contar++;
+            if (numeros > maximo) {
+                maximo = numeros;
+            }
+            System.out.print("¿Desea ingresar otro número? (SI/NO): ");
+            respuesta = datos.next();
+        } while (respuesta.equalsIgnoreCase("SI"));
 
 
+if(contar>0){
+double promedio=(double) sume/contar;
+System.out.println("El numero mas ingresado mayor es: "+maximo);
+System.out.println("El numero mas promediado es: "+promedio);
+}
 
 
+}
+
+public static void Canalesclientes(){
+Scanner datos=new Scanner(System.in);
+System.out.println("Ingrese el nombre del cliente" );
+String nombreCliente= datos.nextLine();
+
+int canalesHD=0;
+int canalesNormales=0;
+
+String respuesta;
+
+do{
+    System.out.println("Ingrese si el canal es hd o normal");
+String Canal=datos.nextLine().toUpperCase();
 
 
+if(Canal.equals("HD")){
+canalesHD++;
 
+}else if(Canal.equals("Normal")){
+canalesNormales++;
+
+}
+ System.out.print("¿Quiere poner otro canal? (SI/NO): ");
+            respuesta = datos.nextLine();
+        } while (respuesta.equalsIgnoreCase("SI"));
+
+System.out.println("Ingrese la caja digital que quiera LIGHTBOX/HDBOX/DVRBOX");
+String caja=datos.nextLine().toUpperCase();
+
+ int subtotal = (canalesNormales * 20) + (canalesHD * 30);
+        if (caja.equals("LIGHTBOX")) {
+            subtotal += 50;
+        } else if (caja.equals("HDBOX")) {
+            subtotal += 100;
+        } else if (caja.equals("DVRBOX")) {
+            subtotal += 150;
+        }
+double impuesto=subtotal*0.15;
+double pagar= subtotal+impuesto;
+
+System.out.println("Detalles");
+System.out.println("Canales Normales: "+canalesNormales);
+System.out.println("Canales HD: "+canalesHD);
+System.out.println("Subtotal: " + subtotal + " Lps");
+ System.out.println("Impuesto (15%): " + impuesto + " Lps");
+System.out.println("Total a Pagar: " + pagar + " Lps");
 
 
 
@@ -83,40 +149,8 @@ int num =datos.nextInt();
 
 
 
-
 }
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
